daa498dcbd5e4399b132585cd733c97ad17a3d6f
임채은 <100753621+im-cherry@users.noreply.github.com>
2022-08-08 22:13:00 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int A = Convert.ToInt32(input[0]);
+            int B = Convert.ToInt32(input[1]);
+            int result = A + B;
+
+            Console.WriteLine(result);
+        }
+    }
+}
