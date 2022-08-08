6c197765f80381941748cf803af61609970b5330
임채은 <100753621+im-cherry@users.noreply.github.com>
2022-08-08 22:44:14 +0900

    [Bronze V] Title: Hello World, Time: 60 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
