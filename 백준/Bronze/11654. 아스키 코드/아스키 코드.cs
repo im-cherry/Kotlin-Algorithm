397e66c717ff83982da52f44b43853fe24d4a815
임채은 <100753621+im-cherry@users.noreply.github.com>
2022-08-12 19:14:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alpha = Console.ReadLine();
+            
+            Console.WriteLine(Convert.ToInt32(alpha[0]));
+        }
+    }
+}
